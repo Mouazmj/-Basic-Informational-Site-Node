9bab4418942c67785202e6e076c2ae71473a56ee
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-10 21:46:22 +0100

    Adding the alternatives

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const http = require('http')
 const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    const path = '/'
+    const path = ''
     switch(req.url) {
         case '/':
-            
+            path += 'index.html';
+            break;
+            case '/about':
+                path += 'about.html';
+                break;
+                case '/contact-me':
+                    path += 'contact.html'
     }
 })
