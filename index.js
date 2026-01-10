773161b5f64cbefe4ba036bbf813aa4822359c85
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-10 21:40:04 +0100

    Require fs and start with switch to find path to html files

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
-    
+    const path = '/'
+    switch(req.url) {
+        case '/':
+            
+    }
 })
