a7f4dc8efa9404cffae23b6d10d6d74239caa8ad
Mouaz Jamal <mj225ih@student.lnu.se>
2026-01-11 22:19:04 +0100

    Using redirect method instead of using status 301 with setHeader location to the real path

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const express = require('express')
 
 const app = express()
 
-app.listen(8080)
+app.listen(3000)
 
 app.get('/', (req, res) => {
-    res.send('<p>Home page</p>')
     res.sendFile('./views/index.html', { root: __dirname })
 })
 
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname })
+})
+
+app.get('/contact-me', (req, res) => {
+    res.sendFile('./views/contact-me.html', { root: __dirname })
+})
+
+app.get('/about-us', (req, res) => {
+    res.redirect('/about')
+})
